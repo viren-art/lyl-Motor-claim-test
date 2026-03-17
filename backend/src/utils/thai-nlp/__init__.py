1e2c6e2f27a4345103a261b7f214f17cfac35fd9
viren-art <virendra.mulkalwar@gmail.com>
2026-03-17 14:59:08 +0700

    chore: initial codebase from LYL Build Agent (42/111)

---
+++ backend/src/utils/thai-nlp/__init__.py
@@ -0,0 +1,7 @@
+"""
+Thai NLP utilities for claim extraction
+"""
+from .language_detector import LanguageDetector
+from .thai_normalizer import ThaiNormalizer
+
+__all__ = ['LanguageDetector', 'ThaiNormalizer']
